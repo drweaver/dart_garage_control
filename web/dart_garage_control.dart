3094779fe094e3c0ab4e4b92eb19eab645852dfe
Shane Weaver <drweaver@gmail.com>
2014-02-17 21:46:01 +0000

    implemented open

--- web/dart_garage_control.dart
+++ web/dart_garage_control.dart
@@ -4,7 +4,7 @@ import 'dart:convert' show JSON;
 
 const REFRESH = const Duration(seconds: 3);
 
-final uriRoot = 'http://192.168.0.20:5110/gc/garagedoor';
+final uriRoot = 'garagedoor';
 
 final stateUri = '${uriRoot}/state';
 final stopUri = '${uriRoot}/stop';
@@ -16,14 +16,25 @@ var refreshCount = 0;
 final ERROR_MAX =  5;
 final REFRESH_MAX = 20;
 
+Geoposition pos = null;
+
 void main() {
+  post(stateUri);
   querySelector("#refresh").onClick.listen((e)=>post(stateUri));
   querySelector("#stop").onClick.listen( (e)=>post(stopUri));
   querySelector('#close').onClick.listen( (e)=>post(closeUri));
-  post(stateUri);
+  querySelector('#open').onClick.listen( (e)=>post('$openUri?${posUriParams()}') );
+  window.navigator.geolocation.watchPosition().listen((Geoposition position) {
+    pos = position;
+    print('Position = ${pos.coords.latitude},${pos.coords.longitude}');
+    (querySelector("#open") as ButtonElement).disabled = false;
+  }, onError: (e)=>print('PositionError: ${e.message}'));
 }
 
+String posUriParams() => 'lat=${pos.coords.latitude}&lng=${pos.coords.longitude}';
+
 void post(String uri) {
+  print('making request to: $uri');
   querySelector('#spinner').classes.remove('hidden');
   var httpRequest = new HttpRequest();
   httpRequest..open('POST', uri)
@@ -52,9 +63,13 @@ void doorStateResponse(HttpRequest request) {
 processDoorState(String responseText) {
   var response = JSON.decode(responseText);
   assert( response is Map );
-  assert( response['auth'] == 'OK' );
+  if( response['auth'] != 'OK' ) {
+    querySelector('#status').classes.add('red');
+    return;
+  }
   querySelector("#status").text = response['state'];
-  if( refreshCount++ < REFRESH_MAX && !(response['state'] == 'closed' || response['state'] == 'opened') ) {   
+  if( refreshCount++ < REFRESH_MAX && 
+      !(response['state'] == 'closed' || response['state'] == 'opened') ) {   
     new Timer(REFRESH,()=>post(stateUri));
   } else {
     refreshCount = 0;
