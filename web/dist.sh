77a802700a310abcd29fff31b0bb1d9addb4e30d
Shane Weaver <drweaver@gmail.com>
2014-02-17 23:50:53 +0000

    icons and fixed zoom

--- web/dist.sh
+++ web/dist.sh
@@ -1,1 +1,1 @@
-zip -r dart_garage_control.zip dart_garage_control.css dart_garage_control.dart dart_garage_control.dart.js spinner.gif packages GarageControl.html
+zip -r dart_garage_control.zip ico dart_garage_control.css dart_garage_control.dart dart_garage_control.dart.js spinner.gif packages GarageControl.html
